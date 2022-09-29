09c6df02a35a3463f2ed595b7f26dc8365a97ba3
Rainscarr <ww.ru13@mail.ru>
2022-09-29 12:52:27 +0300

    add files

---
+++ lesson 3/4.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+def lengthh(a,b,l,N):
+    print(2 * l + (2 * N - 1) * a + 2 * (N - 1) * b)
+
+lengthh(int(input('a: ')), int(input('b: ')), int(input('l: ')), int(input('N: ')))
+ 
---
+++ lesson 3/5.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+def min_number(x,y,z):
+    return(min(x,y,z))
+
+print(min_number(int(input())))
