dc15a80e6464a6f1148a65db0a0ab1c538768766
Rainscarr <ww.ru13@mail.ru>
2022-12-01 16:48:24 +0300

    [change] practice 9

--- lesson 9/1.py
+++ lesson 9/1.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 
-c = 0
-s = 0
-n = 3
-A = []
-for i in range (n):
-    b=[]
-    for j in range (n):
-        print('Enter [', i,' ,', j,'] element')
-        b.append(int(input()))
-    A.append(b)
+def matrix ():
 
-for i in range (n):
-    for j in range (n):
-        print("%2d " % A[i][j], end='')
-    print()
+    c = 0
+    s = 0
+    n = 3
+    A = []
+    for i in range (n):
+        b=[]
+        for j in range (n):
+            print('Enter [', i,' ,', j,'] element')
+            b.append(int(input()))
+        A.append(b)
 
-for i in range(n):
-    for j in range(i + 1, n):
-        if A[i][j] > 0:
-            s += A[i][j]
-            c += 1
-print("Кол-во:",c)
-print("Sum:",s)
+    for i in range (n):
+        for j in range (n):
+            print("%2d " % A[i][j], end='')
+        print()
 
+    for i in range(n):
+        for j in range(i + 1, n):
+            if A[i][j] > 0:
+                s += A[i][j]
+                c += 1
+    print("Кол-во:",c)
+    print("Sum:",s)
 
-
-
+matrix ()
